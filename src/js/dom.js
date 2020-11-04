6c707f61b64367597ffb064e5406b3e807b46604
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-11-04 23:15:25 +0300

    Create Form Dynamically.

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,27 +1,27 @@
 function createForm() {
-  const form = document.createElement("form");
+  const form = document.createElement('form');
 
-  const bookTitleField = document.createElement("input");
-  bookTitleField.setAttribute("id", "title");
-  bookTitleField.setAttribute("type", "text");
-  bookTitleField.setAttribute("placeholder", "Book Title");
+  const bookTitleField = document.createElement('input');
+  bookTitleField.setAttribute('id', 'title');
+  bookTitleField.setAttribute('type', 'text');
+  bookTitleField.setAttribute('placeholder', 'Book Title');
 
-  const bookAuthorField = document.createElement("input");
-  bookAuthorField.setAttribute("id", "author");
-  bookAuthorField.setAttribute("type", "text");
-  bookAuthorField.setAttribute("placeholder", "Book Author");
+  const bookAuthorField = document.createElement('input');
+  bookAuthorField.setAttribute('id', 'author');
+  bookAuthorField.setAttribute('type', 'text');
+  bookAuthorField.setAttribute('placeholder', 'Book Author');
 
-  const bookPagesField = document.createElement("input");
-  bookPagesField.setAttribute("id", "pages");
-  bookPagesField.setAttribute("type", "number");
-  bookPagesField.setAttribute("placeholder", "Book Pages");
+  const bookPagesField = document.createElement('input');
+  bookPagesField.setAttribute('id', 'pages');
+  bookPagesField.setAttribute('type', 'number');
+  bookPagesField.setAttribute('placeholder', 'Book Pages');
 
-  const bookRead = document.createElement("input");
-  bookRead.setAttribute("id", "read");
-  bookRead.setAttribute("type", "checkbox");
+  const bookRead = document.createElement('input');
+  bookRead.setAttribute('id', 'read');
+  bookRead.setAttribute('type', 'checkbox');
 
-  const submitButton = document.createElement("button");
-  submitButton.setAttribute("type", "submit");
+  const submitButton = document.createElement('button');
+  submitButton.setAttribute('type', 'submit');
   submitButton.innerHTML = 'New Book';
 
   form.appendChild(bookTitleField);
